3d971cf30c6a5482f9e1eaaabefd622d98101872
Angelo Orate <ott.regisorate@gmail.com>
2025-02-17 13:14:31 +0800

    Implement MovieRepositoryImpl

---
+++ lib/features/movie/data/repositories/movie_repository_impl.dart
@@ -0,0 +1,21 @@
+import 'package:fpdart/src/either.dart';
+import 'package:movie_mite/core/resources/exceptions.dart';
+import 'package:movie_mite/core/resources/failures.dart';
+import 'package:movie_mite/features/movie/data/datasources/tmdb_datasource.dart';
+import 'package:movie_mite/features/movie/domain/entities/movie_entity.dart';
+import 'package:movie_mite/features/movie/domain/repositories/movie_repository.dart';
+
+final class MovieRepositoryImpl implements MovieRepository {
+  final TmdbDatasource _tmdbDatasource = TmdbDatasource();
+
+  @override
+  Future<Either<Failure, List<MovieEntity>>> getPopularMovies() async {
+    try {
+      final models = await _tmdbDatasource.getPopularMovies();
+      final entities = models.map((e) => e.toEntity()).toList();
+      return Right(entities);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(detail: e.detail));
+    }
+  }
+}
