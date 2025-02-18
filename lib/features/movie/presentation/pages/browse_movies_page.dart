23a2dec51f73683bac0fb333053cdafc52963301
Angelo Orate <ott.regisorate@gmail.com>
2025-02-18 19:37:55 +0800

    Implement movies list on presentation layer

--- lib/features/movie/presentation/pages/browse_movies_page.dart
+++ lib/features/movie/presentation/pages/browse_movies_page.dart
@@ -5,6 +5,7 @@ import 'package:movie_mite/features/movie/data/repositories/movie_repository_imp
 import 'package:movie_mite/features/movie/domain/entities/movie_entity.dart';
 import 'package:movie_mite/features/movie/domain/repositories/movie_repository.dart';
 import 'package:movie_mite/features/movie/presentation/logic/logic.dart';
+import 'package:movie_mite/features/movie/presentation/logic/movie_list/movie_list_bloc.dart';
 
 class BrowseMoviesPage extends StatelessWidget {
   const BrowseMoviesPage({super.key});
@@ -23,11 +24,22 @@ class BrowseMoviesPage extends StatelessWidget {
               ),
         ),
       ],
-      child: BlocProvider(
-        create:
-            (context) => BrowseMoviesBloc(
-              RepositoryProvider.of<MovieRepository>(context),
-            )..add(FetchPopularMovies()),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create:
+                (context) => MovieListBloc(
+                  RepositoryProvider.of<MovieRepository>(context),
+                ),
+          ),
+          BlocProvider(
+            lazy: false,
+            create:
+                (context) => BrowseMoviesBloc(
+                  RepositoryProvider.of<MovieRepository>(context),
+                )..add(FetchPopularMovies()),
+          ),
+        ],
         child: BrowseMoviesScreen(),
       ),
     );
@@ -40,15 +52,24 @@ class BrowseMoviesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-        title: Text("Browse Movies"),
-      ),
       body: RefreshIndicator(
         onRefresh: () {
           return Future.delayed(Durations.long1);
         },
-        child: MovieListBuilder(),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              leading: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.menu),
+              ),
+              title: Text("Browse Movies"),
+            ),
+            // Category(),
+            MovieListBuilder(),
+          ],
+        ),
       ),
     );
   }
@@ -59,32 +80,22 @@ class MovieListBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<BrowseMoviesBloc, BrowseMoviesState>(
+    return BlocBuilder<MovieListBloc, MovieListState>(
       builder: (context, state) {
-        switch (state) {
-          case BrowseMoviesInitial():
-          case BrowseMoviesLoading():
-            return const Center(child: CircularProgressIndicator());
-          case BrowseMoviesFailed():
-            return const Center(child: Text("Something went wrong!"));
-          case BrowseMoviesEmpty():
-            return const Center(child: Text("No movies found!"));
-          case BrowseMoviesLoaded():
-            return MovieList(movies: state.movies);
-        }
+        return MoviesList(movies: state.movies);
       },
     );
   }
 }
 
-class MovieList extends StatelessWidget {
-  const MovieList({super.key, required this.movies});
+class MoviesList extends StatelessWidget {
+  const MoviesList({super.key, required this.movies});
 
   final List<MovieEntity> movies;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return SliverList.builder(
       itemCount: movies.length,
       itemBuilder: (context, index) {
         return ListTile(
@@ -96,13 +107,25 @@ class MovieList extends StatelessWidget {
   }
 }
 
-class MovieCard extends StatelessWidget {
-  const MovieCard({super.key, required this.movie});
-
-  final MovieEntity movie;
+class Category extends StatelessWidget {
+  const Category({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(movie.originalTitle);
+    final colors = [
+      Colors.red,
+      Colors.green,
+      Colors.blue,
+      Colors.yellow,
+      Colors.orange,
+      Colors.purple,
+    ];
+    return SliverToBoxAdapter(
+      child: Column(
+        children: [
+          for (var color in colors) Container(height: 200, color: color),
+        ],
+      ),
+    );
   }
 }
