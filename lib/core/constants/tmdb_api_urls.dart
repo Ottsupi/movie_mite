2005c1e436f399be692dd98e5221d1a7474a1138
Angelo Orate <ott.regisorate@gmail.com>
2025-02-18 22:12:07 +0800

    Update TmdbApiUrls

--- lib/core/constants/tmdb_api_urls.dart
+++ lib/core/constants/tmdb_api_urls.dart
@@ -1,8 +1,10 @@
 final class TmdbApiUrls {
   static const String baseUrl = "https://api.themoviedb.org";
+
+  // Movies
+  static const String nowPlayingMovies = "$baseUrl/3/movie/now_playing";
   static const String popularMovies = "$baseUrl/3/movie/popular";
-  static const String nowPlaying = "$baseUrl/3/movie/nowPlaying";
-  static const String topRated = "$baseUrl/3/movie/topRated";
-  static const String upcoming = "$baseUrl/3/movie/upcoming";
-  static const String trending = "$baseUrl/3/trending/movie/__time_window__";
+  static const String topRatedMovies = "$baseUrl/3/movie/top_rated";
+  static const String trendingMovies = "$baseUrl/3/trending/movie/day";
+  static const String upcomingMovies = "$baseUrl/3/movie/upcoming";
 }
