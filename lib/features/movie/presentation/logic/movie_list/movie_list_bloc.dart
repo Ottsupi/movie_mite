b8cc7f6d822b66f5738411a0c7d7b0eef7febf58
Angelo Orate <ott.regisorate@gmail.com>
2025-02-18 19:32:43 +0800

    Add logging on exceptional errors

--- lib/features/movie/presentation/logic/movie_list/movie_list_bloc.dart
+++ lib/features/movie/presentation/logic/movie_list/movie_list_bloc.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 import 'package:movie_mite/features/movie/domain/entities/movie_entity.dart';
 import 'package:movie_mite/features/movie/domain/repositories/movie_repository.dart';
 
@@ -12,11 +14,14 @@ class MovieListBloc extends Bloc<MovieListEvent, MovieListState> {
   final MovieRepository _movieRepository;
   late StreamSubscription<List<MovieEntity>> _movieListStreamSubscription;
 
+  final logger = GetIt.instance.get<Logger>();
+
   MovieListBloc(this._movieRepository) : super(MovieListState.empty()) {
     on<RecievedMovieList>(_onRecievedMovieList);
 
     _movieListStreamSubscription = _movieRepository.movieListStream().listen(
       (movieList) => add(RecievedMovieList(movieList)),
+      onError: (error) => logger.e(error),
     );
   }
 
