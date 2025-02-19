3e4dd45ad38d242f04c80b70ed6faaf1aa949509
Angelo Orate <ott.regisorate@gmail.com>
2025-02-19 11:04:10 +0800

    Revise approach to BrowseMoviesBloc

--- lib/features/movie/presentation/logic/browse_movies/browse_movies_bloc.dart
+++ lib/features/movie/presentation/logic/browse_movies/browse_movies_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:movie_mite/features/movie/domain/entities/movie_entity.dart';
+import 'package:movie_mite/core/resources/failures.dart';
+import 'package:movie_mite/features/movie/domain/repositories/enums/movie_collection_enums.dart';
 import 'package:movie_mite/features/movie/domain/repositories/movie_repository.dart';
 import 'package:movie_mite/features/movie/domain/use_cases/get_popular_movies.dart';
 
@@ -10,7 +11,7 @@ part 'browse_movies_state.dart';
 class BrowseMoviesBloc extends Bloc<BrowseMoviesEvent, BrowseMoviesState> {
   final MovieRepository _movieRepository;
 
-  BrowseMoviesBloc(this._movieRepository) : super(BrowseMoviesInitial()) {
+  BrowseMoviesBloc(this._movieRepository) : super(BrowseMoviesState.initial()) {
     on<FetchPopularMovies>(_onGetPopularMovies);
   }
 
@@ -18,13 +19,7 @@ class BrowseMoviesBloc extends Bloc<BrowseMoviesEvent, BrowseMoviesState> {
     FetchPopularMovies event,
     Emitter<BrowseMoviesState> emit,
   ) async {
-    emit(BrowseMoviesLoading());
-    final result = await GetPopularMovies(
-      _movieRepository,
-    ).call(Params(page: 1));
-    result.fold(
-      (failure) => emit(BrowseMoviesFailed()),
-      (movies) => emit(BrowseMoviesLoaded(movies: movies)),
-    );
+    emit(BrowseMoviesState(page: 1, collection: MovieCollection.popular));
+    await GetPopularMovies(_movieRepository).call(Params(page: 1));
   }
 }
--- lib/features/movie/presentation/logic/browse_movies/browse_movies_state.dart
+++ lib/features/movie/presentation/logic/browse_movies/browse_movies_state.dart
@@ -1,22 +1,33 @@
 part of 'browse_movies_bloc.dart';
 
-sealed class BrowseMoviesState extends Equatable {
-  const BrowseMoviesState();
+final class BrowseMoviesState extends Equatable {
+  const BrowseMoviesState({
+    required this.page,
+    required this.collection,
+    this.failure,
+  });
+
+  final int page;
+  final MovieCollection collection;
+
+  final Failure? failure;
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [page, collection, failure];
+
+  factory BrowseMoviesState.initial() {
+    return BrowseMoviesState(page: 1, collection: MovieCollection.popular);
+  }
+
+  BrowseMoviesState copyWith({
+    int? page,
+    MovieCollection? collection,
+    Failure? failure,
+  }) {
+    return BrowseMoviesState(
+      page: page ?? this.page,
+      collection: collection ?? this.collection,
+      failure: failure ?? this.failure,
+    );
+  }
 }
-
-final class BrowseMoviesInitial extends BrowseMoviesState {}
-
-final class BrowseMoviesLoading extends BrowseMoviesState {}
-
-final class BrowseMoviesLoaded extends BrowseMoviesState {
-  final List<MovieEntity> movies;
-
-  const BrowseMoviesLoaded({required this.movies});
-}
-
-final class BrowseMoviesFailed extends BrowseMoviesState {}
-
-final class BrowseMoviesEmpty extends BrowseMoviesState {}
