b89f493238593b5f2805a5a388db1a382561fb2d
Angelo Orate <ott.regisorate@gmail.com>
2025-02-22 21:17:54 +0800

    Add movieListStatus stream

--- lib/features/movie/domain/repositories/favorite_repository.dart
+++ lib/features/movie/domain/repositories/favorite_repository.dart
@@ -1,6 +1,7 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:movie_mite/core/resources/failures.dart';
 import 'package:movie_mite/features/movie/domain/entities/movie_entity.dart';
+import 'package:movie_mite/features/movie/domain/repositories/enums/movie_list_status.dart';
 
 abstract class FavoriteRepository {
   /// Get a list of all favorite movies
@@ -11,6 +12,9 @@ abstract class FavoriteRepository {
 
   /// Add movie to favorite
   Future<Either<Failure, void>> removeFavoriteMovie(MovieEntity movie);
+
+  /// Stream of [MovieListStatus]
+  Stream<MovieListStatus> movieListStatus();
 
   /// Stream of a list of [MovieEntity]
   Stream<List<MovieEntity>> movieListStream();
