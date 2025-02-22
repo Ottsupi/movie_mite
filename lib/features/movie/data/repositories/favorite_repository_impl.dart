94916fa07fd9d34d8f3d1d14e5ed07d8f12d683d
Angelo Orate <ott.regisorate@gmail.com>
2025-02-22 21:18:10 +0800

    Implement FavoriteRepositoryImpl

---
+++ lib/features/movie/data/repositories/favorite_repository_impl.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:fpdart/src/either.dart';
+import 'package:movie_mite/core/resources/failures.dart';
+import 'package:movie_mite/features/movie/data/datasources/favorite_datasource.dart';
+import 'package:movie_mite/features/movie/data/models/drift_movie_model.dart';
+import 'package:movie_mite/features/movie/domain/entities/movie_entity.dart';
+import 'package:movie_mite/features/movie/domain/repositories/enums/movie_list_status.dart';
+import 'package:movie_mite/features/movie/domain/repositories/favorite_repository.dart';
+
+final class FavoriteRepositoryImpl implements FavoriteRepository {
+  FavoriteRepositoryImpl(this._favoriteDatasource);
+
+  final FavoriteDatasource _favoriteDatasource;
+
+  final _movieListStatusController =
+      StreamController<MovieListStatus>.broadcast();
+  final _movieListStreamController =
+      StreamController<List<MovieEntity>>.broadcast();
+
+  @override
+  Future<Either<Failure, List<MovieEntity>>> getFavoriteMovies() async {
+    try {
+      final result = await _favoriteDatasource.getFavoriteMovies();
+      return Right(result.map((e) => e.toEntity()).toList());
+    } on CacheFailure catch (e) {
+      return Left(CacheFailure(detail: e.detail));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> addFavoriteMovie(MovieEntity movie) async {
+    try {
+      await _favoriteDatasource.addFavoriteMovie(
+        DriftMovieModel.fromEntity(movie),
+      );
+      return Right(null);
+    } on CacheFailure catch (e) {
+      return Left(CacheFailure(detail: e.detail));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> removeFavoriteMovie(MovieEntity movie) async {
+    try {
+      await _favoriteDatasource.getFavoriteMovieBySourceId(
+        source: movie.source,
+        sourceId: movie.sourceId,
+      );
+      return Right(null);
+    } on CacheFailure catch (e) {
+      return Left(CacheFailure(detail: e.detail));
+    }
+  }
+
+  @override
+  Stream<MovieListStatus> movieListStatus() {
+    return _movieListStatusController.stream;
+  }
+
+  @override
+  Stream<List<MovieEntity>> movieListStream() {
+    return _movieListStreamController.stream;
+  }
+
+  @override
+  Future<void> dispose() async {
+    await _movieListStatusController.close();
+    await _movieListStreamController.close();
+  }
+}
