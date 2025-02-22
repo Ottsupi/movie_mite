6972b2c9b8485d6736a9563acbf15804afd537d7
Angelo Orate <ott.regisorate@gmail.com>
2025-02-22 10:31:01 +0800

    Add tabview for browse movies

---
+++ lib/features/movie/presentation/pages/browse_movies_tabs_page.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
+import 'package:movie_mite/features/movie/data/datasources/tmdb_datasource.dart';
+import 'package:movie_mite/features/movie/data/repositories/movie_repository_impl.dart';
+import 'package:movie_mite/features/movie/domain/repositories/enums/movie_collection_enums.dart';
+import 'package:movie_mite/features/movie/domain/repositories/movie_repository.dart';
+import 'package:movie_mite/features/movie/presentation/logic/browse_movies/browse_movies_bloc.dart';
+import 'package:movie_mite/features/movie/presentation/logic/movie_list/movie_list_bloc.dart';
+import 'package:movie_mite/features/movie/presentation/logic/movie_list_status/movie_list_status_bloc.dart';
+import 'package:movie_mite/features/movie/presentation/widgets/movie_list_builder.dart';
+import 'package:movie_mite/features/movie/presentation/widgets/movie_list_status_builder.dart';
+import 'package:rxdart/rxdart.dart';
+
+class BrowseMoviesTabsPage extends StatelessWidget {
+  const BrowseMoviesTabsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider<MovieRemoteDatasource>(
+      create: (context) => TmdbDatasource(),
+      child: BrowseMoviesTabsScreen(),
+    );
+  }
+}
+
+class BrowseMoviesTabsScreen extends StatelessWidget {
+  const BrowseMoviesTabsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: MovieCollection.values.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Browse Movies'),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              for (MovieCollection collection in MovieCollection.values)
+                Tab(text: collection.name),
+            ],
+          ),
+        ),
+        body: BrowseCollectionTabView(),
+      ),
+    );
+  }
+}
+
+class BrowseCollectionTabView extends StatelessWidget {
+  const BrowseCollectionTabView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+      children: [
+        for (MovieCollection collection in MovieCollection.values)
+          BrowseCollectionTab(collection: collection),
+      ],
+    );
+  }
+}
+
+class BrowseCollectionTab extends StatefulWidget {
+  const BrowseCollectionTab({super.key, required this.collection});
+
+  final MovieCollection collection;
+
+  @override
+  State<BrowseCollectionTab> createState() => _BrowseCollectionTabState();
+}
+
+class _BrowseCollectionTabState extends State<BrowseCollectionTab> {
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider<MovieRepository>(
+      create:
+          (context) => MovieRepositoryImpl(
+            RepositoryProvider.of<MovieRemoteDatasource>(context),
+          ),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<MovieListBloc>(
+            create:
+                (context) => MovieListBloc(
+                  RepositoryProvider.of<MovieRepository>(context),
+                ),
+          ),
+          BlocProvider<BrowseMoviesBloc>(
+            lazy: false,
+            create:
+                (context) => BrowseMoviesBloc(
+                  RepositoryProvider.of<MovieRepository>(context),
+                )..add(
+                  FetchMoviesByCollection(
+                    page: 1,
+                    collection: widget.collection,
+                  ),
+                ),
+          ),
+          BlocProvider<MovieListStatusBloc>(
+            create:
+                (context) => MovieListStatusBloc(
+                  RepositoryProvider.of<MovieRepository>(context),
+                ),
+          ),
+        ],
+        child: BrowseCollection(collection: widget.collection),
+      ),
+    );
+  }
+}
+
+class BrowseCollection extends StatefulWidget {
+  const BrowseCollection({super.key, required this.collection});
+
+  final MovieCollection collection;
+
+  @override
+  State<BrowseCollection> createState() => _BrowseCollectionState();
+}
+
+class _BrowseCollectionState extends State<BrowseCollection> {
+  final _scrollController = ScrollController();
+  final _scrollSubject = PublishSubject<void>();
+  final logger = GetIt.I<Logger>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(() {
+      _scrollSubject.add(null);
+    });
+
+    _scrollSubject.debounceTime(const Duration(milliseconds: 500)).listen((_) {
+      if (_scrollController.position.atEdge &&
+          _scrollController.position.pixels > 0) {
+        logger.d('Fetch next page!');
+        context.read<BrowseMoviesBloc>().add(FetchNextPage());
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(() {
+      _scrollSubject.add(null);
+    });
+    _scrollController.dispose();
+    _scrollSubject.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () {
+        return Future.delayed(Durations.extralong4);
+      },
+      child: CustomScrollView(
+        controller: _scrollController,
+        slivers: [MovieListBuilder(), MovieListStatusBuilder()],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_mite/core/network/api_client.dart';
 import 'package:movie_mite/core/utils/logs/logger.dart';
 import 'package:movie_mite/features/movie/presentation/pages/browse_movies_page.dart';
+import 'package:movie_mite/features/movie/presentation/pages/browse_movies_tabs_page.dart';
 
 void main() {
   setupLogger();
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const BrowseMoviesPage(),
+      home: const BrowseMoviesTabsPage(),
     );
   }
 }
