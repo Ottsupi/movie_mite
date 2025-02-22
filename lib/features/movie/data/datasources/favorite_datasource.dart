59df2d6d5278e6b0c9a8c7ec5236681ef709ef0b
Angelo Orate <ott.regisorate@gmail.com>
2025-02-22 19:55:35 +0800

    Add FavoriteDatasource

--- lib/features/movie/data/datasources/favorite_datasource.dart
+++ lib/features/movie/data/datasources/favorite_datasource.dart
@@ -1,4 +1,8 @@
+import 'package:drift/drift.dart';
+import 'package:get_it/get_it.dart';
 import 'package:movie_mite/core/database/drift_database.dart';
+import 'package:movie_mite/core/resources/exceptions.dart';
+import 'package:movie_mite/features/movie/data/models/drift_movie_model.dart';
 
 abstract class FavoriteDatasource {
   /// Get a list of all favorite movies
@@ -8,25 +12,62 @@ abstract class FavoriteDatasource {
   Future<void> addFavoriteMovie(DriftMovieModel movie);
 
   /// Remove movie from favorites
-  Future<void> removeFavoriteMovie(DriftMovieModel movie);
+  Future<void> removeFavoriteMovieById(int id);
+
+  /// Get by source id
+  Future<DriftMovieModel?> getFavoriteMovieBySourceId(
+    String sourceId,
+    String source,
+  );
 }
 
 final class FavoriteDatasourceImpl implements FavoriteDatasource {
+  final db = GetIt.I<AppDatabase>();
+
   @override
   Future<List<DriftMovieModel>> getFavoriteMovies() async {
-    // TODO: implement addFavoriteMovie
-    throw UnimplementedError();
+    try {
+      final results = await db.select(db.driftMovieTable).get();
+      return results.map((e) => DriftMovieModel.fromTableData(e)).toList();
+    } on Exception catch (e) {
+      throw CacheException(detail: 'Could not get favorite movies', error: e);
+    }
   }
 
   @override
-  Future<void> addFavoriteMovie(DriftMovieModel movie) {
-    // TODO: implement addFavoriteMovie
-    throw UnimplementedError();
+  Future<void> addFavoriteMovie(DriftMovieModel movie) async {
+    try {
+      await db.into(db.driftMovieTable).insert(movie.toCompanion());
+    } on Exception catch (e) {
+      throw CacheException(detail: 'Could not add favorite movie', error: e);
+    }
   }
 
   @override
-  Future<void> removeFavoriteMovie(DriftMovieModel movie) {
-    // TODO: implement removeFavoriteMovie
-    throw UnimplementedError();
+  Future<void> removeFavoriteMovieById(int id) async {
+    try {
+      await db.delete(db.driftMovieTable)
+        ..where((movie) => movie.id.equals(id));
+    } on Exception catch (e) {
+      throw CacheException(detail: 'Could not remove favorite movie', error: e);
+    }
+  }
+
+  @override
+  Future<DriftMovieModel?> getFavoriteMovieBySourceId(
+    String sourceId,
+    String source,
+  ) async {
+    try {
+      final result =
+          await (db.select(db.driftMovieTable)..where(
+            (movie) =>
+                movie.sourceId.equals(sourceId) & movie.source.equals(source),
+          )).getSingleOrNull();
+      if (result == null) return null;
+      return DriftMovieModel.fromTableData(result);
+    } on Exception catch (e) {
+      throw CacheException(detail: 'Could not find favorite movie', error: e);
+    }
   }
 }
