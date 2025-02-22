537400ba1e4f7f1b24083c0e0508910301314efd
Angelo Orate <ott.regisorate@gmail.com>
2025-02-22 15:13:03 +0800

    Fix to not use Either

--- lib/features/movie/data/datasources/favorite_datasource.dart
+++ lib/features/movie/data/datasources/favorite_datasource.dart
@@ -1,33 +1,31 @@
-import 'package:fpdart/fpdart.dart';
 import 'package:movie_mite/core/database/drift_database.dart';
-import 'package:movie_mite/core/resources/failures.dart';
 
 abstract class FavoriteDatasource {
   /// Get a list of all favorite movies
-  Future<Either<Failure, List<DriftMovieModel>>> getFavoriteMovies();
+  Future<List<DriftMovieModel>> getFavoriteMovies();
 
   /// Add movie to favorites
-  Future<Either<Failure, void>> addFavoriteMovie(DriftMovieModel movie);
+  Future<void> addFavoriteMovie(DriftMovieModel movie);
 
   /// Remove movie from favorites
-  Future<Either<Failure, void>> removeFavoriteMovie(DriftMovieModel movie);
+  Future<void> removeFavoriteMovie(DriftMovieModel movie);
 }
 
 final class FavoriteDatasourceImpl implements FavoriteDatasource {
   @override
-  Future<Either<Failure, List<DriftMovieModel>>> getFavoriteMovies() {
-    // TODO: implement getFavoriteMovies
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, void>> addFavoriteMovie(DriftMovieModel movie) {
+  Future<List<DriftMovieModel>> getFavoriteMovies() async {
     // TODO: implement addFavoriteMovie
     throw UnimplementedError();
   }
 
   @override
-  Future<Either<Failure, void>> removeFavoriteMovie(DriftMovieModel movie) {
+  Future<void> addFavoriteMovie(DriftMovieModel movie) {
+    // TODO: implement addFavoriteMovie
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> removeFavoriteMovie(DriftMovieModel movie) {
     // TODO: implement removeFavoriteMovie
     throw UnimplementedError();
   }
