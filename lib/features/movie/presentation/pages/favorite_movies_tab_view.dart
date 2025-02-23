470958d9453277ba6ca3ca3d735cac27a95ca68d
Angelo Orate <ott.regisorate@gmail.com>
2025-02-23 13:07:25 +0800

    Implement FavoritesMoviesTabView

--- lib/features/movie/presentation/pages/favorite_movies_tab_view.dart
+++ lib/features/movie/presentation/pages/favorite_movies_tab_view.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_mite/features/movie/presentation/logic/favorite_movies/favorite_movies_bloc.dart';
+import 'package:movie_mite/features/movie/data/datasources/favorite_datasource.dart';
+import 'package:movie_mite/features/movie/data/datasources/tmdb_datasource.dart';
+import 'package:movie_mite/features/movie/data/repositories/movie_repository_impl.dart';
+import 'package:movie_mite/features/movie/domain/repositories/enums/movie_collection_enums.dart';
+import 'package:movie_mite/features/movie/domain/repositories/movie_repository.dart';
+import 'package:movie_mite/features/movie/presentation/logic/browse_movies/browse_movies_bloc.dart';
+import 'package:movie_mite/features/movie/presentation/logic/movie_list/movie_list_bloc.dart';
+import 'package:movie_mite/features/movie/presentation/logic/movie_list_status/movie_list_status_bloc.dart';
+import 'package:movie_mite/features/movie/presentation/widgets/movie_list_builder.dart';
 
 class FavoriteMoviesTabView extends StatefulWidget {
   const FavoriteMoviesTabView({super.key});
@@ -11,14 +19,43 @@ class FavoriteMoviesTabView extends StatefulWidget {
 
 class _FavoriteMoviesTabViewState extends State<FavoriteMoviesTabView> {
   @override
-  void initState() {
-    super.initState();
-    BlocProvider.of<FavoriteMoviesBloc>(context).add(FetchFavoriteMovies());
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return FavoriteMovies();
+    return RepositoryProvider<MovieRepository>(
+      create:
+          (context) => MovieRepositoryImpl(
+            RepositoryProvider.of<MovieRemoteDatasource>(context),
+            RepositoryProvider.of<FavoriteDatasource>(context),
+          ),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<MovieListBloc>(
+            create:
+                (context) => MovieListBloc(
+                  RepositoryProvider.of<MovieRepository>(context),
+                ),
+          ),
+          BlocProvider<BrowseMoviesBloc>(
+            lazy: false,
+            create:
+                (context) => BrowseMoviesBloc(
+                  RepositoryProvider.of<MovieRepository>(context),
+                )..add(
+                  FetchMoviesByCollection(
+                    page: 1,
+                    collection: MovieCollection.favorite,
+                  ),
+                ),
+          ),
+          BlocProvider<MovieListStatusBloc>(
+            create:
+                (context) => MovieListStatusBloc(
+                  RepositoryProvider.of<MovieRepository>(context),
+                ),
+          ),
+        ],
+        child: FavoriteMovies(),
+      ),
+    );
   }
 }
 
@@ -28,16 +65,10 @@ class FavoriteMovies extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
-      onRefresh: () async {
-        context.read<FavoriteMoviesBloc>().add(FetchFavoriteMovies());
+      onRefresh: () {
+        return Future.delayed(Durations.extralong4);
       },
-      child: CustomScrollView(
-        slivers: [
-          SliverFillRemaining(
-            child: Container(child: Center(child: Text('Favorites'))),
-          ),
-        ],
-      ),
+      child: CustomScrollView(slivers: [MovieListBuilder()]),
     );
   }
 }
