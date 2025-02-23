a2011924d82b72c6201c62708b0183c7b3c346ad
Angelo Orate <ott.regisorate@gmail.com>
2025-02-23 12:35:07 +0800

    Add favorite to MovieCollectionEnum

--- lib/features/movie/domain/repositories/enums/movie_collection_enums.dart
+++ lib/features/movie/domain/repositories/enums/movie_collection_enums.dart
@@ -1,8 +1,16 @@
 /// Collection of movies
-enum MovieCollection { popular, nowShowing, topRated, trending, upcoming }
+enum MovieCollection {
+  favorite,
+  popular,
+  nowShowing,
+  topRated,
+  trending,
+  upcoming,
+}
 
 extension MovieCollectionExt on MovieCollection {
   String get name => switch (this) {
+    MovieCollection.favorite => 'Favorites',
     MovieCollection.nowShowing => 'Now Showing',
     MovieCollection.popular => 'Popular',
     MovieCollection.topRated => 'Top Rated',
