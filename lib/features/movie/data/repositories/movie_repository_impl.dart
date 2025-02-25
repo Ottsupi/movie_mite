978b7250dfed2e7d6279c58a78129a6ee46920e9
Angelo Orate <ott.regisorate@gmail.com>
2025-02-25 19:49:51 +0800

    Implement injection of favorites movies to getMoviesByCollection

--- lib/features/movie/data/repositories/movie_repository_impl.dart
+++ lib/features/movie/data/repositories/movie_repository_impl.dart
@@ -7,6 +7,7 @@ import 'package:movie_mite/core/resources/exceptions.dart';
 import 'package:movie_mite/core/resources/failures.dart';
 import 'package:movie_mite/features/movie/data/datasources/favorite_datasource.dart';
 import 'package:movie_mite/features/movie/data/datasources/tmdb_datasource.dart';
+import 'package:movie_mite/features/movie/data/models/tmdb_movie_model.dart';
 import 'package:movie_mite/features/movie/domain/entities/movie_entity.dart';
 import 'package:movie_mite/features/movie/domain/repositories/enums/movie_collection_enums.dart';
 import 'package:movie_mite/features/movie/domain/repositories/enums/movie_list_status.dart';
@@ -17,6 +18,7 @@ final class MovieRepositoryImpl implements MovieRepository {
 
   final MovieRemoteDatasource _remoteMovieDatasource;
   final FavoriteDatasource _favoriteDatasource;
+  final _logger = GetIt.instance.get<Logger>();
 
   final _movieListStatusController =
       StreamController<MovieListStatus>.broadcast();
@@ -46,24 +48,35 @@ final class MovieRepositoryImpl implements MovieRepository {
     MovieCollection collection,
     int page,
   ) async {
+    if (collection == MovieCollection.favorite) return getFavoriteMovies();
+    if (page == 1) _movieListStatusController.add(MovieListStatus.initial);
+
+    // Start fetching the movies
+    _movieListStatusController.add(MovieListStatus.loading);
+
+    // Retrieve from remote
+    List<TmdbMovieModel> models = [];
     try {
-      if (collection == MovieCollection.favorite) {
-        return await getFavoriteMovies();
-      }
-      if (page == 1) _movieListStatusController.add(MovieListStatus.initial);
-      _movieListStatusController.add(MovieListStatus.loading);
-      final models = await _remoteMovieDatasource.getMoviesByCollection(
+      models = await _remoteMovieDatasource.getMoviesByCollection(
         collection,
         page,
       );
-      final entities = models.map((e) => e.toEntity()).toList();
-      _movieListStreamController.add(entities);
-      _movieListStatusController.add(MovieListStatus.networkLoaded);
-      return Right(entities);
     } on ServerException catch (e) {
       _movieListStatusController.add(MovieListStatus.error);
       return Left(ServerFailure.fromServerException(e));
     }
+
+    // Convert to entities
+    final entities = models.map((e) => e.toEntity()).toList();
+
+    // Inject movies already in favorites
+    final injection = await _injectFavorites(entities);
+    final finalList = injection.fold((failure) => entities, (movies) => movies);
+
+    // Return
+    _movieListStreamController.add(finalList);
+    _movieListStatusController.add(MovieListStatus.networkLoaded);
+    return Right(finalList);
   }
 
   @override
@@ -116,5 +129,29 @@ final class MovieRepositoryImpl implements MovieRepository {
   Future<void> dispose() async {
     await _movieListStatusController.close();
     await _movieListStreamController.close();
+  }
+
+  /// Injects favorite movies into a given list.
+  Future<Either<Failure, List<MovieEntity>>> _injectFavorites(
+    List<MovieEntity> movies,
+  ) async {
+    try {
+      List<MovieEntity> injected = [];
+      for (final movie in movies) {
+        final match = await _favoriteDatasource.getFavoriteMovieBySourceId(
+          source: movie.source,
+          sourceId: movie.sourceId,
+        );
+        if (match != null) {
+          injected.add(match.toEntity());
+        } else {
+          injected.add(movie);
+        }
+      }
+      return Right(injected);
+    } on CacheException catch (e) {
+      _logger.e("Unable to inject favorite movies: $e");
+      return Left(CacheFailure(detail: e.detail));
+    }
   }
 }
