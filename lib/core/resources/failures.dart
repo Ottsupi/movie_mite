1f74c0cb130933ed735b5134dd5573b30eac6d90
Angelo Orate <ott.regisorate@gmail.com>
2025-02-25 16:12:08 +0800

    Add method ServerFailure.fromDioException

--- lib/core/resources/failures.dart
+++ lib/core/resources/failures.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
@@ -12,6 +13,32 @@ abstract class Failure extends Equatable {
 // General failures
 class ServerFailure extends Failure {
   ServerFailure({required String detail}) : super(detail: detail);
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.sendTimeout:
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(
+          detail: "Server took too long to respond. Please try again later.",
+        );
+      case DioExceptionType.badCertificate:
+        return ServerFailure(detail: "Bad certificate.");
+      case DioExceptionType.badResponse:
+        return ServerFailure(
+          detail: "Something went wrong. Please try again later.",
+        );
+      case DioExceptionType.cancel:
+        return ServerFailure(detail: "Request to server was cancelled.");
+      case DioExceptionType.connectionError:
+        return ServerFailure(
+          detail:
+              "Could not connect to the server. Please check your internet connection and try again later.",
+        );
+      case DioExceptionType.unknown:
+        return ServerFailure(detail: "Unknown error.");
+    }
+  }
 }
 
 class CacheFailure extends Failure {
