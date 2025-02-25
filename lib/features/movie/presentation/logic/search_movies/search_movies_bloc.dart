12f63371d074eb0cda5a046fc44348d1cb5acd0d
Angelo Orate <ott.regisorate@gmail.com>
2025-02-25 15:09:49 +0800

    Add new SearchMoviesProperty property SearchMoviesStatus status

--- lib/features/movie/presentation/logic/search_movies/search_movies_bloc.dart
+++ lib/features/movie/presentation/logic/search_movies/search_movies_bloc.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 import 'package:movie_mite/core/resources/failures.dart';
 import 'package:movie_mite/features/movie/domain/repositories/movie_repository.dart';
 import 'package:movie_mite/features/movie/domain/use_cases/search_movie_by_title.dart';
@@ -9,6 +11,7 @@ part 'search_movies_state.dart';
 
 class SearchMoviesBloc extends Bloc<SearchMoviesEvent, SearchMoviesState> {
   final MovieRepository _movieRepository;
+  final _logger = GetIt.I.get<Logger>();
 
   static const int maxPages = 2;
   static const int itemsPerPage = 20;
@@ -23,7 +26,13 @@ class SearchMoviesBloc extends Bloc<SearchMoviesEvent, SearchMoviesState> {
     FetchMovieByTitle event,
     Emitter<SearchMoviesState> emit,
   ) async {
-    emit(SearchMoviesState(page: event.page, title: event.title));
+    emit(
+      SearchMoviesState(
+        page: event.page,
+        status: SearchMoviesStatus.loading,
+        title: event.title,
+      ),
+    );
     final params = SearchMovieByTitleParams(
       page: event.page,
       title: event.title,
@@ -31,18 +40,39 @@ class SearchMoviesBloc extends Bloc<SearchMoviesEvent, SearchMoviesState> {
     final result = await SearchMovieByTitle(_movieRepository).call(params);
     result.fold(
       (failure) {
-        emit(state.copyWith(page: event.page - 1, failure: failure));
+        emit(
+          state.copyWith(
+            page: event.page - 1,
+            status: SearchMoviesStatus.failed,
+            failure: failure,
+          ),
+        );
       },
       (movies) {
-        emit(
-          state.copyWith(page: event.page, title: event.title, failure: null),
+        SearchMoviesState newState = state.copyWith(
+          page: event.page,
+          title: event.title,
+          status: SearchMoviesStatus.loaded,
+          failure: null,
         );
+        if (movies.isEmpty) {
+          newState = newState.copyWith(status: SearchMoviesStatus.empty);
+          // Edge case when search results is divisible by [itemsPerPage]
+          if (event.page > 1) {
+            _logger.d("Empty and last page...");
+            newState = newState.copyWith(status: SearchMoviesStatus.lastPage);
+          }
+        } else if (movies.length < itemsPerPage) {
+          newState = newState.copyWith(status: SearchMoviesStatus.lastPage);
+        }
+        emit(newState);
       },
     );
   }
 
   _onFetchNextPage(FetchNextPage event, Emitter<SearchMoviesState> emit) {
     if (state.page >= maxPages) return;
+    if (state.status.isLastPage) return;
     add(FetchMovieByTitle(page: state.page + 1, title: state.title));
   }
 
--- lib/features/movie/presentation/logic/search_movies/search_movies_state.dart
+++ lib/features/movie/presentation/logic/search_movies/search_movies_state.dart
@@ -1,24 +1,51 @@
 part of 'search_movies_bloc.dart';
 
+enum SearchMoviesStatus { initial, loading, loaded, empty, lastPage, failed }
+
+extension SearchMoviesStatusIsX on SearchMoviesStatus {
+  bool get isInitial => this == SearchMoviesStatus.initial;
+  bool get isLoading => this == SearchMoviesStatus.loading;
+  bool get isLoaded => this == SearchMoviesStatus.loaded;
+  bool get isEmpty => this == SearchMoviesStatus.empty;
+  bool get isLastPage => this == SearchMoviesStatus.lastPage;
+  bool get isFailed => this == SearchMoviesStatus.failed;
+}
+
 final class SearchMoviesState extends Equatable {
-  SearchMoviesState({required this.page, required this.title, this.failure});
+  SearchMoviesState({
+    required this.page,
+    required this.title,
+    required this.status,
+    this.failure,
+  });
 
   final int page;
   final String title;
+  final SearchMoviesStatus status;
 
   final Failure? failure;
 
   @override
-  List<Object?> get props => [page, title, failure];
+  List<Object?> get props => [page, title, status, failure];
 
   factory SearchMoviesState.initial() {
-    return SearchMoviesState(page: 0, title: '');
+    return SearchMoviesState(
+      page: 0,
+      status: SearchMoviesStatus.initial,
+      title: '',
+    );
   }
 
-  SearchMoviesState copyWith({int? page, String? title, Failure? failure}) {
+  SearchMoviesState copyWith({
+    int? page,
+    String? title,
+    SearchMoviesStatus? status,
+    Failure? failure,
+  }) {
     return SearchMoviesState(
       page: page ?? this.page,
       title: title ?? this.title,
+      status: status ?? this.status,
       failure: failure ?? this.failure,
     );
   }
